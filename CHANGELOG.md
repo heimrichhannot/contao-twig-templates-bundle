13d3df676b5aec98d6022988b6792ced0dc0a23c
Thomas Körner <t.koerner@heimrich-hannot.de>
2020-09-15 16:45:06 +0200

    fixed repo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [1.5.1] - 2020-09-15
+- fixed repo and added changes from 1.5 into master branch
+- from 1.5: added placeholder for date/time fields (accessibility)
+
 ## [1.4.4] - 2020-09-15
 - added errorClass variable to multiple module templates
 - added error block to multiple variable templates
--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,14 +1,18 @@
 <?php
 
-/**
- * Hooks
+/*
+ * Copyright (c) 2020 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
  */
-$GLOBALS['TL_HOOKS']['parseTemplate']['twig-templates']    = ['huh.twig.event_listener.hook_listener', 'parseTemplate'];
+
+$GLOBALS['TL_HOOKS']['parseTemplate']['twig-templates'] = ['huh.twig.event_listener.hook_listener', 'parseTemplate'];
+$GLOBALS['TL_HOOKS']['getAttributesFromDca']['twig-templates'] = [\HeimrichHannot\TwigTemplatesBundle\EventListener\GetAttributesFromDcaListener::class, '__invoke'];
 
 if (is_array($GLOBALS['TL_HOOKS']['parseWidget']) && in_array(['HeimrichHannot\FormHybrid\Hooks', 'parseWidgetHook'], $GLOBALS['TL_HOOKS']['parseWidget'])) {
     array_insert($GLOBALS['TL_HOOKS']['parseWidget'], array_search(['HeimrichHannot\FormHybrid\Hooks', 'parseWidgetHook'], $GLOBALS['TL_HOOKS']['parseWidget']), [
-        'twig-templates' => ['huh.twig.event_listener.hook_listener', 'parseWidget']
+        'twig-templates' => ['huh.twig.event_listener.hook_listener', 'parseWidget'],
     ]);
 } else {
-    $GLOBALS['TL_HOOKS']['parseWidget']['twig-templates']    = ['huh.twig.event_listener.hook_listener', 'parseWidget'];
+    $GLOBALS['TL_HOOKS']['parseWidget']['twig-templates'] = ['huh.twig.event_listener.hook_listener', 'parseWidget'];
 }
